ec4ddd63753669fcfb22fa3c98d1df83ac3bf78a
gch2126 Grace Huh <gch2126@columbia.edu>
2023-10-04 05:24:56 +0000

    testing

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,7 +9,7 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
     @all_ratings = Movie.all_ratings  
-    @ratings_to_show = Array.empty
+    @ratings_to_show = Array.new
   end
 
   def new
