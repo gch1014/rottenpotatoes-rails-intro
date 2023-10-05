93f90a0705bbb1a724c31ba3e03e9654413b43d1
gch2126 Grace Huh <gch2126@columbia.edu>
2023-10-05 17:21:24 +0000

    checkboxes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -24,7 +24,7 @@ class MoviesController < ApplicationController
     if ratings_checked.present?
       @ratings_to_show = ratings_checked.keys
     else
-      @ratings_to_show = Array.new
+      @ratings_to_show = @all_ratings.keys
     end
 
     @movies = Movie.with_ratings(@ratings_to_show)
